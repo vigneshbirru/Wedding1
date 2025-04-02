242a29bf65fd85bc1c85d2c8c4022927d1b79a38
VighneshBirru <vignesh23birru@gmail.com>
2025-04-02 19:54:51 +0530

    Location

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
-// import Location from './Components/Location'
+import Location from './Components/Location'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
-      {/* <Location/> */}
+      <Location/>
     </div>
   )
 }
